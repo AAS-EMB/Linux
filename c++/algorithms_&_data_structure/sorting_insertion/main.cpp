713848b95ba5121c003b96244628c40fafdfe364
Anton <antoha123k@yandex.ru>
2021-06-04 16:36:47 +0300

    Sources were fixed to clang format

--- c++/algorithms_&_data_structure/sorting_insertion/main.cpp
+++ c++/algorithms_&_data_structure/sorting_insertion/main.cpp
@@ -2,44 +2,45 @@
 
 void SortingInsertion(int *pArray, unsigned int ArraySize);
 
-int main(int argc, char *argv[])
-{
-	unsigned int ArraySize;
+int main(int argc, char *argv[]) {
+  unsigned int ArraySize;
 
-	std::cout << "\r\nSet the size of the array: ";
-	std::cin >> ArraySize;
-	int *Array = new int[ArraySize];
-	std::cout << "\r\nEnter the values of the array elements" << std::endl;
-	for(auto Iterator = &Array[0]; Iterator != (&Array[0] + ArraySize); Iterator++) {
-		std::cin >> *Iterator;
-	}
-	SortingInsertion(Array, ArraySize);
-	std::cout << "\r\nSorted array by insertion method" << std::endl;
-	for(auto Iterator = &Array[0]; Iterator != (&Array[0] + ArraySize); Iterator++) {
-		std::cout << "" << *Iterator << " ";
-	}
-	std::cout << std::endl;
-	delete [] Array;
-	return 0;
+  std::cout << "\r\nSet the size of the array: ";
+  std::cin >> ArraySize;
+  int *Array = new int[ArraySize];
+  std::cout << "\r\nEnter the values of the array elements" << std::endl;
+  for (auto Iterator = &Array[0]; Iterator != (&Array[0] + ArraySize);
+       Iterator++) {
+    std::cin >> *Iterator;
+  }
+  SortingInsertion(Array, ArraySize);
+  std::cout << "\r\nSorted array by insertion method" << std::endl;
+  for (auto Iterator = &Array[0]; Iterator != (&Array[0] + ArraySize);
+       Iterator++) {
+    std::cout << "" << *Iterator << " ";
+  }
+  std::cout << std::endl;
+  delete[] Array;
+  return 0;
 }
 
 /*!
  * @brief The sort method of the insertion
- * 
+ *
  * @param pArray    Pointer to the object
  * @param ArraySize Size object
  */
-void SortingInsertion(int *pArray, unsigned int ArraySize)
-{
-	int TempValue, PrevIndex;
+void SortingInsertion(int *pArray, unsigned int ArraySize) {
+  int TempValue, PrevIndex;
 
-	for(unsigned int CurrentIndex = 1; CurrentIndex < ArraySize; CurrentIndex++) {
-		TempValue = pArray[CurrentIndex];
-		PrevIndex = CurrentIndex - 1;
-		while(PrevIndex >= 0 && pArray[PrevIndex] > TempValue) {
-			pArray[PrevIndex + 1] = pArray[PrevIndex];
-			pArray[PrevIndex] = TempValue;
-			PrevIndex--;
-		}
-	}
+  for (unsigned int CurrentIndex = 1; CurrentIndex < ArraySize;
+       CurrentIndex++) {
+    TempValue = pArray[CurrentIndex];
+    PrevIndex = CurrentIndex - 1;
+    while (PrevIndex >= 0 && pArray[PrevIndex] > TempValue) {
+      pArray[PrevIndex + 1] = pArray[PrevIndex];
+      pArray[PrevIndex] = TempValue;
+      PrevIndex--;
+    }
+  }
 }
